5c46015ed37892a821ea2bf7e7ff678f2d351eea
gargl791 <glenmarcogarcia123@gmail.com>
2024-12-25 03:15:06 +1300

    add mongodb mongoose connnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const dbUri = process.env.MONGODB_URI;
+
+if(!dbUri) {
+  throw new Error("DB_URI missing in .env file.")
+}
 
 // Middleware
 app.use(cors());
@@ -15,6 +21,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello from Node.js backend!');
 });
+
+mongoose.connect(dbUri)
+
 
 // Start server
 app.listen(port, () => {
