71e5cb5053bdd6aab898a68fd911a3f0d0084469
Glen Garcia <gargl791@student.otago.ac.nz>
2024-12-25 14:10:08 +1300

    issue with ts datatypes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,16 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
-const app = express();
+const app: Application = express();
 const port = process.env.PORT || 5000;
 const dbUri = process.env.MONGODB_URI;
 
-if(!dbUri) {
-  throw new Error("DB_URI missing in .env file.")
+if (!dbUri) {
+  throw new Error('DB_URI missing in .env file.');
 }
 
 // Middleware
@@ -18,12 +18,17 @@ app.use(cors());
 app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Node.js backend!');
 });
 
 mongoose.connect(dbUri)
-
+  .then(() => {
+    console.log('Successfully connected to MongoDB database');
+  })
+  .catch((err: Error) => {
+    console.error('Error in connecting to MongoDB database', err);
+  });
 
 // Start server
 app.listen(port, () => {
